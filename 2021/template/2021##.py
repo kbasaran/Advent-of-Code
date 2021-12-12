cc13c1a926aa01b3ad4e281eb3bd6cc741142355
kbasaran <kbasaran@gmail.com>
2021-12-12 19:43:14 +0100

    Update 2021##.py

--- 2021/template/2021##.py
+++ 2021/template/2021##.py
@@ -9,4 +9,26 @@ start_time = perf_counter()
 
 # Code here
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+print(f"\n--Part 1--\nAnswer: {var1}")
+print(f"\n--Part 2--\nAnswer: {var2}")
 print(f"\nSolved in {(perf_counter() - start_time) * 1000:.1f} ms.")
