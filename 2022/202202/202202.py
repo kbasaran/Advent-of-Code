d56f2812947592006aa0d371d03ba88bfd68d16d
kbasaran <kbasaran@gmail.com>
2022-12-02 22:29:44 +0100

    solved part 2

--- 2022/202202/202202.py
+++ 2022/202202/202202.py
@@ -7,10 +7,9 @@ with open('input.txt') as f:
 from time import perf_counter
 import numpy as np
 
-
 start_time = perf_counter()
 
-
+# rows are opponent's hand, columns are your
 win_mat = np.matrix([[3, 6, 0],
                      [0, 3, 6],
                      [6, 0, 3],
@@ -24,13 +23,22 @@ def convert_letters_to_coords(row_text):
     return row_no, col_no
 
 
+# Part 1
 tot_score = 0
 for row_text in p_in.splitlines():
     tot_score += win_mat[convert_letters_to_coords(row_text)]
-
 print(tot_score)
 
 # Part 2
+score_mat = np.matrix([[3, 1, 2],
+                       [1, 2, 3],
+                       [2, 3, 1],
+                       ]) + np.matrix([0, 3, 6]).repeat(3, axis=0)
+
+tot_score = 0
+for row_text in p_in.splitlines():
+    tot_score += score_mat[convert_letters_to_coords(row_text)]
+print(tot_score)
 
 
 print(f"\nSolved in {(perf_counter() - start_time) * 1000:.1f} ms.")
