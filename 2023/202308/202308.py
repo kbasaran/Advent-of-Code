d7c6ca271ecce1fe72a2fd40db7ceb90094386b6
kbasaran <kbasaran@proton.me>
2023-12-10 13:03:52 +0100

    Solved part 2

--- 2023/202308/202308.py
+++ 2023/202308/202308.py
@@ -8,51 +8,57 @@ with open('input.txt') as f:
 from time import perf_counter
 from itertools import product
 import pandas as pd
-
-start_time = perf_counter()
+import math
 
 # ---- Part 1
+start_time = perf_counter()
 
 letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
-def convert_dir_to_number(dir_in: str) -> int:
-    return int(dir_in.replace("L", "1").replace("R", "2"), base=3)
-
-def create_memory_array():
-    column_names = ["".join(letters) for letters in product(letters, repeat=3)]
-    memory = pd.DataFrame(columns=column_names, index=[1,2])
+def create_memory_array(repeat):
+    column_names = ["".join(letters) for letters in product(letters, repeat=repeat)]
+    memory = pd.DataFrame(columns=column_names, index=["L", "R"], dtype=str).fillna(0)
     return memory
 
 def parse_input(p_in):
-    array = create_memory_array()
+    array = create_memory_array(3)
     directions, connections = p_in.strip().split("\n\n")
     for line in connections.splitlines():
         loc, dirs = line.split(" = ")
         dir_left, dir_right = dirs.replace("(", "").replace(")", "").split(", ")
-        array.at[1, loc] = dir_left
-        array.at[2, loc] = dir_right
+        array.at["L", loc] = dir_left
+        array.at["R", loc] = dir_right
     return array, directions
 
-def get_repeated_string(seed_string, start, end):
-    if start > end:
-        raise ValueError("Invalid range: start should be less than or equal to end")
+def get_pos_from_repeated_string(seed_string, pos):
     len_seed = len(seed_string)
-    reduce = start // len_seed
-    start -= len_seed * reduce
-    end -= len_seed * reduce
-    periods = end // len_seed + 1
-    raw = seed_string * periods
-
-    return raw[start:end]
+    reduce = pos // len_seed
+    pos -= len_seed * reduce
+    return seed_string[pos]
 
 memory, directions = parse_input(p_in)
 pos, loc = 0, "AAA"
 while loc != "ZZZ":
-    instruction = get_repeated_string(directions, pos, pos+1)
+    instruction = get_pos_from_repeated_string(directions, pos)
     # print(f"Handling instruction {instruction}")
-    instruction_as_number = convert_dir_to_number(instruction)
-    loc = memory.at[instruction_as_number, loc]
+    loc = memory.at[instruction, loc]
     pos += 1
 
 print(f"Part 1: {pos}")
 print(f"Solved in {(perf_counter() - start_time) * 1000:.3g} ms.\n")
+
+# ---- Part 2
+start_time = perf_counter()
+
+start_locations = memory.loc[:, memory.columns.str.endswith("A") & (memory != 0).any(axis=0)].columns
+states = {start_location: (0, start_location) for start_location in start_locations}
+pos = 0
+while any([loc[2] != "Z" for _, loc in states.values()]):
+    instruction = get_pos_from_repeated_string(directions, pos)
+    for start_location, (_, loc) in states.items():
+        if loc[2] != "Z":
+            states[start_location] = (pos + 1, memory.at[instruction, loc])
+    pos += 1
+
+print(f"Part 2:\n{states}\nLCM: {math.lcm(*[val[0] for val in states.values()])}")
+print(f"Solved in {(perf_counter() - start_time) * 1000:.3g} ms.")
