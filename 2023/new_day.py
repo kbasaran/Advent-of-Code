7a6e251be195c405fa1c24394eb2500f7b8860b2
kbasaran <kbasaran@proton.me>
2023-12-10 13:03:32 +0100

    Update new_day.py

--- 2023/new_day.py
+++ 2023/new_day.py
@@ -44,16 +44,15 @@ def make_folder(day):
     if not os.path.exists(file_path):
         with open(file_path, "w") as f:
             f.write(f"# {year} Day {str(day).zfill(2)}"
-"""\n\n# Receive input
+"""\n\n# ---- Receive input
 # with open('input.txt') as f:
 with open("test.txt") as f:
     p_in = f.read()
 
 from time import perf_counter
 
-start_time = perf_counter()
-
 # ---- Part 1
+start_time = perf_counter()
 
 
 
